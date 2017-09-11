0923efc9092cce034075ad4c44186a1c111e3469
Bryce Hill <bryce@clearvoice.com>
2017-09-10 19:59:21 -0700

    Use local webpack binary to allow for various webpack versions

--- bin/tag-builds.js
+++ bin/tag-builds.js
@@ -39,56 +39,46 @@ git.tags((err, { all }) => {
               if (err) return console.error(chalk.red(err, stderr))
               process.env.NODE_ENV = 'production'
               // TODO: Get from args
-              const conf = require(`${CWD}/config/webpack.config.js`)
-              const config =
-                typeof conf === 'function' ? conf({ app: 'publisher' }) : conf
+              const configPath = `${CWD}/config/webpack.config.js`
               console.log('Building with webpack')
               // TODO: Create module that builds webpack and spits out the entry point
               // bundle gzipped sizes. For use in a GH bot???
-              webpack(config, (err, stats) => {
-                if (err) return console.error(err)
-                const json = stats.toJson({
-                  assets: true,
-                  cached: false,
-                  cachedAssets: false,
-                  chunks: false,
-                  chunkModules: false,
-                  publicPath: true,
-                  modules: false
-                })
+              exec(
+                `./node_modules/.bin/webpack --config ${configPath} -j --display-entrypoints --env.app=publisher`,
+                { cwd: CWD, maxBuffer: Infinity },
+                (err, stdout, stderr) => {
+                  if (err) return console.error(err)
+                  const json = JSON.parse(stdout)
+                  console.log(chalk.green('Bundle built'))
+                  json.assets.forEach(asset => {
+                    Object.keys(json.entrypoints).forEach(name => {
+                      const ep = json.entrypoints[name]
+                      const re = /\.(js|css)$/
 
-                if (stats.hasErrors()) {
-                  console.error(chalk.red(json.errors))
-                  return cb()
-                }
-                if (stats.hasWarnings())
-                  console.warn(chalk.yellow(json.warnings))
-
-                console.log(chalk.green('Bundle built'))
-                json.assets.forEach(asset => {
-                  Object.keys(json.entrypoints).forEach(name => {
-                    const ep = json.entrypoints[name]
-                    const re = /\.(js|css)$/
-
-                    if (ep.assets.includes(asset.name) && re.test(asset.name)) {
-                      console.log(`${asset.name} - ${asset.size}`)
-                      const p = path.join(
-                        CWD,
-                        // Get public???
-                        'public',
-                        json.publicPath,
-                        asset.name
-                      )
-                      const file = fs.readFileSync(p, 'utf8')
-                      data[tag].push({
-                        name: asset.name,
-                        size: prettyBytes(gzipSize.sync(file))
-                      })
-                    }
+                      if (
+                        ep.assets.includes(asset.name) &&
+                        re.test(asset.name)
+                      ) {
+                        const p = path.join(
+                          CWD,
+                          // Get public??? or build into specific folder?
+                          'public',
+                          json.publicPath,
+                          asset.name
+                        )
+                        const file = fs.readFileSync(p, 'utf8')
+                        const size = prettyBytes(gzipSize.sync(file))
+                        console.log(`${asset.name} - ${size}`)
+                        data[tag].push({
+                          name: asset.name,
+                          size
+                        })
+                      }
+                    })
                   })
-                })
-                cb()
-              })
+                  cb()
+                }
+              )
             }
           )
         })
